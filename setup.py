fa43be5e13768753a9874b72ec75f611980540dc
cloomis <cloomis@localhost>
2009-10-21 22:43:07 +0000

    comment out extension: it causes installation to happen to the wrong place.

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ import sdss3tools
 import os
 
 sdss3tools.setup(
-        ext_modules=[Extension('gimg', 
-                               sources=['src/gimg/ipGguide.c', 'src/gimg/gutils.c'],
-                               include_dirs=['include'],
-                               )],
+#        ext_modules=[Extension('gimg', 
+#                               sources=['src/gimg/ipGguide.c', 'src/gimg/gutils.c'],
+#                               include_dirs=['include'],
+#                               )],
 
         description = "SDSS-3 guider actor.",
         )
