bdc1b7847c5fdb9e3184bb63a5630d6f0cae0327
cloomis <cloomis@localhost>
2010-11-01 07:50:35 +0000

    rephrase how we fetch cartridge info.

--- python/guiderActor/loadGprobes.py
+++ python/guiderActor/loadGprobes.py
@@ -1,13 +1,37 @@
 import math
+import numpy
+import os
+import subprocess
+import tempfile
 from opscore.utility.YPF import YPF
 
-def getGprobes(fiberinfofn, plugmapfn, cartridge):
+from guiderActor import *
+
+def getDefGprobes(plateId, cartId):
+	if not 'GUIDERACTOR_DIR' in os.environ:
+		raise RuntimeError('guiderActor product must be setup')
+	if not 'PLATEDB_DIR' in os.environ:
+		raise RuntimeError('platedb product must be setup')
+
+	fiberinfoFile = os.path.join(os.environ.get('GUIDERACTOR_DIR'), 'etc', 'gcamFiberInfo.par')
+	scratchFile = tempfile.NamedTemporaryFile()
+
+	args = ['catPlPlugMapM']
+	if cartId:
+		args.extend(['-c', str(cartId)])
+	args.append(str(plateId))
+	#args.extend(['>>', scratchFile.name])
+	subprocess.check_call(args, stdout=scratchFile)
+
+	return getGprobes(fiberinfoFile, scratchFile.name, cartId)
+
+def getGprobes(fiberinfofn, plugmapfn, cartId):
 	par = YPF(fiberinfofn)
 	probes = par.structs['GPROBE'].asObjlist()
-	probes = [p for p in probes if p.cartridgeId == cartridge]
+	probes = [p for p in probes if p.cartridgeId == cartId]
 	guideprobes = [p for p in probes if p.fiberType == 'GUIDE' or p.fiberType == 'ACQUIRE']
 	tritiumprobes = [p for p in probes if p.fiberType == 'TRITIUM']
-
+	
 	plugmapfile = YPF(plugmapfn)
 	plugmap = plugmapfile.structs['PLUGMAPOBJ'].asObjlist()
 	aholes = [p for p in plugmap if p.holeType == 'ALIGNMENT']
@@ -21,18 +45,11 @@ def getGprobes(fiberinfofn, plugmapfn, cartridge):
 
 	gprobes = {}
 	for (p, ghole, ahole) in zip(guideprobes + tritiumprobes, gholes + [None], aholes + [None]):
-		info = ducky()
-		info.enabled = True
-		info.exists = p.exists
-		info.fiber_type = p.fiberType
-		info.flags = 0
-		info.xCenter = p.xcen
-		info.yCenter = p.ycen
-		info.radius = p.radius
-		info.xFerruleOffset = p.xferruleOffset
-		info.yFerruleOffset = p.yferruleOffset
-		info.rotation = p.rot
-		info.focusOffset = p.focusOffset
+		info = GprobeInfo(p.exists, True,
+						  p.xcen, p.ycen, p.radius, p.rot,
+						  p.xferruleOffset, p.yferruleOffset,
+						  p.focusOffset, p.fiberType, 0)
+
 		if p.fiberType in ['GUIDE', 'ACQUIRE']:
 			info.ra  = ghole.ra
 			info.dec = ghole.dec
@@ -53,3 +70,7 @@ def getGprobes(fiberinfofn, plugmapfn, cartridge):
 
 		gprobes[p.gProbeId] = info
 	return gprobes
+
+if __name__ == "__main__":
+	pass
+
