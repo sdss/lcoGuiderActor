7cc72828880b0707d35f570d437e98ead828e985
cloomis <cloomis@d97-176.lco.cl>
2010-11-01 07:49:04 +0000

    use the probe closest to the center of the plate as the PtErrProbe

--- python/guiderActor/convertPlPlugMap.py
+++ python/guiderActor/convertPlPlugMap.py
@@ -47,22 +47,23 @@ typedef struct {
 """
 
 def writeGProbe(cartInfo, probeInfo):
-    if len(cartInfo) != 1 or len(probeInfo) != 1:
-        import pdb; pdb.set_trace()
-            
+    """ Write out a single GProbe entry for a GCView block. """
+
     mmToDeg = 1/217.7358
-    pcen = numpy.array([cartInfo.xcen, cartInfo.ycen])
-    prad = cartInfo.radius * math.sqrt(2.0)
-    print "! radius = %0.1f" % (cartInfo.radius)
-    print "GProbe  %d T" % (probeInfo.fiberId)
+    pcen = numpy.array([cartInfo['xcen'], cartInfo['ycen']])
+    prad = cartInfo['radius'] * math.sqrt(2.0)
+    print "! radius = %0.1f" % (cartInfo['radius'])
+    print "GProbe  %d T" % (probeInfo['fiberId'])
     print "    %0.1f %0.1f" % (pcen[0], pcen[1])
     print "    %0.1f %0.1f" % (pcen[0]-prad, pcen[1]-prad)
     print "    %0.1f %0.1f" % (pcen[0]+prad, pcen[1]+prad)
-    print "    %0.9f %0.9f" % (-probeInfo.yFocal * mmToDeg, probeInfo.xFocal * mmToDeg)
+    print "    %0.9f %0.9f" % (-probeInfo['yFocal'] * mmToDeg, probeInfo['xFocal'] * mmToDeg)
     print "    0.0 ! the 25m guider code handles the fiber rotation"
     print ""
 
 def cvtPlugMap(plugFile):
+    """ Write out an entire GCView block for the given plugfile. """
+
     ypm = YPF.YPF(plugFile)
     pm = ypm.structs['PLUGMAPOBJ'].asArray()
 
@@ -71,22 +72,36 @@ def cvtPlugMap(plugFile):
     cartId = ypm.vars['cartridgeId'].value
     cartInfo = getCartInfo(cartId)
 
-    # Find the best acquisition probe. CPL
-    ptErrProbe = 11
-    #import pdb; pdb.set_trace()
-    print "PtErrProbe %d" % (ptErrProbe)
-    print
+    # Find the best acquisition probe. The small acquisition fibers are not marked as such,
+    # so use the radius.
+    acqProbes = cartInfo[(cartInfo['exists'] > 0) & (cartInfo['radius'] > 14)]
+    
+    # Use the closest one to the plate center. 
+    dMin = 9999.9
+    pMin = None
+    for p in acqProbes:
+        f = gfibers[gfibers['fiberId'] == p['gProbeId']]
+        d = numpy.sqrt(f['xFocal']**2 + f['yFocal']**2)
+        if d < dMin:
+            dMin = d
+            pMin = p
 
+    if pMin == None:
+        raise RuntimeError("no available acquisition fiber")
 
     print "! cart = %d; plate = %d" % (cartId, ypm.vars['plateId'].value)
-    print "! n(map probes) = %d; n(cart probes) = %d" % (len(gfibers), len(cartInfo))
+    print
+    print "PtErrProbe %d" % (pMin['gProbeId'])
+    print
 
-    for i in range(len(gfibers)):
-        f = gfibers[i:i+1]
-        probeInfo = cartInfo[numpy.where(cartInfo.gProbeId == f.fiberId)]
-        writeGProbe(probeInfo, f)
+    for f in gfibers:
+        probeInfo = cartInfo[numpy.where(cartInfo['gProbeId'] == f['fiberId'])]
+        if probeInfo['exists'] > 0: 
+            writeGProbe(probeInfo, f)
 
 def getCartInfo(cartID):
+    """ Return the per-cartidge guider probe info. """
+
     yci = YPF.YPF(os.path.join(os.environ['GUIDERACTOR_DIR'], 'etc', 'gcamFiberInfo.par'))
     allCartInfo = yci.structs['GPROBE'].asArray()
     
@@ -98,88 +113,6 @@ def main():
     
     cvtPlugMap(plugfile)
 
-instblock_template = """
-!+
-! SDSS Engineering Spectrograph.
-! The guide probe data must be specified separately.
-!-
-
-! Instrument
-
-! image center (x,y unbinned pix) and scale (x,y unbinned pix/deg on sky)
-! center is desired loc. of zero boresight; it need not be geometric center
-IIm_Ctr         384       256
-IIm_Scale     23722     23722
-! minimum and maximum instrument image coordinates (x,y unbinned pix)
-IIm_MinXY         0         0
-IIm_MaxXY       767       511
-
-! focus (secondary piston) offset due to instrument (um)
-Inst_Foc          1400                 !Changed 000329 DL
-! position of the center of the rotator in instrument frame (x,y deg)
-Rot_Inst_xy       0.0         0.0      !Changed 040421 DL
-! angle from the instrument x axis to the rotator x axis (deg)
-Rot_Inst_ang    -89.938                !Changed 060825 DL
-!Restored after rotator offset corrected
-
-! minimum and maximum rotator angle due to the instrument (deg)
-! note: the actual rotator limits are the intersection of this
-! with the RotLim angle limits
-InstPosLim     -999       999
-! Guide Camera Image
-
-! ID: controller number (0 if no controller, <0 invalid)
-! and device number (controller-specific)
-! (see file config.txt on the guider Macintosh for camera
-! definitions)
-! 20020606 PRN Changed from device#2 to device#4 to get
-!              the old 40e-/ADU inverse gain.
-GCamID            1         6  !set to high gain 20021113 DL
-! view number
-GCViewNum         0
-! maximum counts per pixel
-GIm_MaxCount   65535
-! default center of guide image (unbinned pix)
-! (for GCAM commands; used when user does not specify center)
-GIm_Ctr         384       256
-! guide image scale (unbinned pix/deg on sky)
-! 60.6 microns/arcsec, 9 micron pixels
-GIm_Scale     23722     23722
-! minimum and maximum position on guide image (unbinned pix)
-GIm_MinXY         0         0
-GIm_MaxXY       767       511
-
-! binning factor (integer x,y)
-GIm_BinFac        2         2
-
-! Guide Probe(s)
-! specify this data in a view file, since it varies by plug-plate
-
-! number of probe used for pointing error measurements
-PtErrProbe   16
-! the minimum # of guide stars needed before rotation
-! or scale will be corrected; values <2 are treated as 2
-MinStarsForRot  3
-
-! Guide Camera Mechanical
-! (focus and filters)
-
-! ID: controller number (0 if no controller, <0 invalid)
-! and device number (controller-specific)
-GMechID           0         0
-! nominal focus setting (piston) for this camera (um)
-! net focus = nominal focus + user focus + filter-specific focus correction
-GCNomFocus        0
-! mininum and maximum guide camera focus (piston) (um)
-GCFocLim          0         0
-! index to currently selected guide camera filter
-! must be in range [1,GCNFilt] if gcNFilt > 0, else ignored
-! warning: index is the position in the list of filters, it is not the filter number
-GCCurrFiltInd               0
-! number of guide camera filters available
-! must be in range [0,12]; 0 if no filter, 1 if one fixed filter
-GCNFilt                     0
-"""
 
 if __name__ == "__main__":
     main()
