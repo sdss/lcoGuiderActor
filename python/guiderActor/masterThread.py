14281c27c478eb798ddda6f325f9e4e02d602b1b
cloomis <cloomis@localhost>
2010-11-17 22:47:39 +0000

    tabnanny

--- python/guiderActor/masterThread.py
+++ python/guiderActor/masterThread.py
@@ -357,14 +357,14 @@ def guideStep(actor, queues, cmd, inFile, oneExposure,
                 #import pdb; pdb.set_trace()
                 pass
 
-		if True:
-            refmag = numpy.nan
-            guideCmd.inform("probe=%d,%2d,0x%02d, %7.2f,%7.2f, %7.3f,%4.0f, %7.2f,%6.2f,%6.2f, %7.2f,%6.2f" % (
-                frameNo, fiber.fiberid, probe.flags,
-                3600.0*(fiber.dRA/gState.plugPlateScale), 3600.0*(fiber.dDec/gState.plugPlateScale),
-                fiber.fwhm, probe.focusOffset,
-                fiber.flux, fiber.mag, refmag, fiber.sky, fiber.skymag))
-
+         if True:
+			 refmag = numpy.nan
+			 guideCmd.inform("probe=%d,%2d,0x%02d, %7.2f,%7.2f, %7.3f,%4.0f, %7.2f,%6.2f,%6.2f, %7.2f,%6.2f" % (
+				 frameNo, fiber.fiberid, probe.flags,
+				 3600.0*(fiber.dRA/gState.plugPlateScale), 3600.0*(fiber.dDec/gState.plugPlateScale),
+				 fiber.fwhm, probe.focusOffset,
+				 fiber.flux, fiber.mag, refmag, fiber.sky, fiber.skymag))
+			 
             print "%d %2d  %7.2f %7.2f  %7.2f %7.2f  %6.1f %6.1f  %6.1f %6.1f  %6.1f %6.1f  %06.1f  %7.3f %7.3f %7.3f %7.3f %4.0f" % (
                 frameNo,
                 fiber.fiberid, dRA, dDec, fiber.dx, fiber.dy, fiber.xs, fiber.ys, fiber.xcen, fiber.ycen,
