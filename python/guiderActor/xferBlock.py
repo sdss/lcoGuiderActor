6cbd2c5766f46dc366481d302e1b178e218f5a56
cloomis <cloomis@localhost>
2010-12-22 04:02:49 +0000

    add xfer-to-tcc routine

---
+++ python/guiderActor/xferBlock.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import os
+import sys
+from ftplib import FTP
+
+tcc = 'tcc25m-p.apo.nmsu.edu'
+
+def xferFile(pathname):
+	dir, fname = os.path.split(pathname)
+	os.chdir(dir)
+	try:
+		ftp = FTP(tcc)
+		ftp.login(user='tcc', passwd='no$$change')
+		ftp.cwd('tinst')
+		ftp.storlines('STOR %s' % (fname), open(fname))
+		ftp.close()
+		print("updated %s on %s" % (fname, tcc))
+	except:
+		print("failed to copy %s to %s" % (fname, tcc))
+
+def main():
+	fname = sys.argv[-1]
+	xferFile(fname)
+	
+if __name__ == "__main__":
+	main()
+	
