f5551653c7023989e9e7000479ffcbf8c9a9266f
parejkoj <parejkoj@d97-176.lco.cl>
2013-02-11 19:16:39 +0000

    Fix tab->spaces consisency.

--- bin/bad_gimg_search.py
+++ bin/bad_gimg_search.py
@@ -11,34 +11,33 @@ from optparse import OptionParser, OptionGroup
 import sys
 
 def do_one_dir(directory,outfile):
-	"""Scan the files in one directory."""
-	files = glob.glob(directory+'/gimg-*.fits')
-	for f in sorted(files):
-		data = pyfits.open(f)[0].data
-		if data.mean() > 20000 and data.std() < 2000:
-			print 'Potentially bad:',f
-			outfile.write(f+'\n')
-			outfile.flush()
+    """Scan the files in one directory."""
+    files = glob.glob(directory+'/gimg-*.fits')
+    for f in sorted(files):
+        data = pyfits.open(f)[0].data
+    if data.mean() > 20000 and data.std() < 2000:
+        print 'Potentially bad:',f
+        outfile.write(f+'\n')
+        outfile.flush()
 #...
 
 def main(argv=None):
-	if argv is None: argv = sys.argv[1:]
-	usage = '%prog [OPTIONS] DIR1 [DIR2 [DIR3 ...]]'
-	usage += '\n\nDIR example: /data/gcam/5623*'
-	parser = OptionParser(usage)
-	parser.add_option('--outfile',dest='outfile',default='bad_gimg.txt',help='write bad file list to this (%default)')
-	# need options?
-	(opts,args) = parser.parse_args(args=argv)
-
-	if len(args) == 0:
-		parser.error('need to pass some directories')
-	else:
-		directories = args
-	
-	outfile = open(opts.outfile,'w')
-	for directory in directories:
-		print 'processing:',directory
-		do_one_dir(directory,outfile)
+    if argv is None: argv = sys.argv[1:]
+    usage = '%prog [OPTIONS] DIR1 [DIR2 [DIR3 ...]]'
+    usage += '\n\nDIR example: /data/gcam/5623*'
+    parser = OptionParser(usage)
+    parser.add_option('--outfile',dest='outfile',default='bad_gimg.txt',help='write bad file list to this (%default)')
+    # need options?
+    (opts,args) = parser.parse_args(args=argv)
+    if len(args) == 0:
+        parser.error('need to pass some directories')
+    else:
+        directories = args
+    
+    outfile = open(opts.outfile,'w')
+    for directory in directories:
+        print 'processing:',directory
+        do_one_dir(directory,outfile)
 #...
 
 if __name__ == "__main__":
