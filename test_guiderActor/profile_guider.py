e22b67efdfe5a3d201365b1c932691d5ac102e34
parejkoj <parejkoj@localhost>
2015-12-01 06:22:57 +0000

    bring the profiling code up to the current TestHelper stuff.

--- test_guiderActor/profile_guider.py
+++ test_guiderActor/profile_guider.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 """Test the speed of guider image processing."""
 import cProfile
+import pstats
 import os
 
 from actorcore import TestHelper
 
 from guiderActor import masterThread
-from guiderActor.gimg import guiderImage
-from guiderActor import GuiderState
 import guiderActor.myGlobals as myGlobals
 
 import guiderTester
@@ -16,24 +15,24 @@ def updateModel(name,model):
     """Update the named actorState model with new parameters."""
     myGlobals.actorState.models[name] = TestHelper.Model(name,model)
 
-darkFile = 'data/gimg-0001.fits.gz'
-flatFile = 'data/gimg-0003.fits.gz'
-dataFile = 'data/gimg-0040.fits.gz'
+darkFile = 'gcam/gimg-0001.fits.gz'
+flatFile = 'gcam/gimg-0003.fits.gz'
+dataFile = 'gcam/gimg-0040.fits.gz'
 
 helper = guiderTester.GuiderTester()
 helper.setUp()
 actor = TestHelper.FakeActor('guider','guiderActor')
-updateModel('mcp',TestHelper.mcpState['science'])
+updateModel('mcp',TestHelper.mcpState['boss_science'])
 helper.actorState.bypassDark = False
 
 # pre-process the dark and flat.
 helper.gi.analyzeDark(darkFile,cmd=helper.cmd)
 helper.gi.analyzeFlat(flatFile,helper.gState.gprobes,cmd=helper.cmd)
-helper.gState.guideCmd = helper.cmd
+helper.gState.cmd = helper.cmd
 
 def timeit_helper():
     masterThread.guideStep(actor,None,helper.cmd,helper.gState,dataFile,False,helper.gi)
-    os.remove('data/proc-gimg-0040.fits.gz')
+    os.remove('gcam/proc-gimg-0040.fits.gz')
 
 # Paste this line for a multi-pass timing run in ipython, after pasting the above.
 # %timeit timeit_helper()
@@ -43,3 +42,6 @@ prof = cProfile.Profile()
 #result = prof.runcall(helper.gi,helper.cmd,dataFile,helper.gState.gprobes,-40)
 result = prof.runcall(masterThread.guideStep,actor,None,helper.cmd,helper.gState,dataFile,False,helper.gi)
 prof.dump_stats('guideStep.profile')
+stats = pstats.Stats('guideStep.profile')
+stats.strip_dirs()
+stats.sort_stats('cumtime').print_stats(20)
