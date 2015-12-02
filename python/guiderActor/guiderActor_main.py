2ce674f50586af53c836914b0def2207508bf91a
parejkoj <parejkoj@d97-176.lco.cl>
2015-12-02 03:31:18 +0000

    Add LCOHACK comments so we know what to change.

--- python/guiderActor/guiderActor_main.py
+++ python/guiderActor/guiderActor_main.py
@@ -5,5 +5,7 @@ from guiderActor import Msg, GuiderActor
 
 # start a new actor
 if __name__ == "__main__":
+    #LCOHACK: since we don't have a proper domain name yet, 
+    #LCOHACK: force the location to lco.
     guider = GuiderActor.GuiderActor.newActor(location='lco')
     guider.run(Msg=Msg)
