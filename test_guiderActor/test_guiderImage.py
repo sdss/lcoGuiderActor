d090e8d47376cc35158e5522340a16de5296bfd9
parejkoj <parejkoj@d97-176.lco.cl>
2015-12-02 01:45:50 +0000

    Nearly working PyGuide replacement for C code, with partial tests.
    Rotation is now done via scipy.ndimage.interpolation.rotate, which seems to
    work, but I need to check if the direction is right.
    
    stars are found via PyGuide.findStars and then .starShape. Still need to
    figure out which values really match between the C code and PyGuide's results.
    
    Needs better unittests: see the TBDs in test_guiderImage.

--- test_guiderActor/test_guiderImage.py
+++ test_guiderActor/test_guiderImage.py
@@ -5,6 +5,7 @@ Test the behavior of guider flats, including finding fibers..
 import os
 import unittest
 import pyfits
+import numpy as np
 
 from actorcore import TestHelper
 
@@ -36,8 +37,15 @@ class TestGuiderImage(guiderTester.GuiderTester,unittest.TestCase):
         self.inDataFile,self.outDataFile = _inout_names('gcam','gimg-0040.fits.gz')
 
         # values for comparison after processing good data files.
-        self.inDataResults = 'something.fits'
+        self.flatExpect = 'gcam/expect-gimg-0003.fits.gz'
+        self.dataExpect = 'gcam/expect-gimg-0040.fits.gz'
+
         super(TestGuiderImage,self).setUp()
+        self._init_probes()
+
+    def tearDown(self):
+        if hasattr(self,'outFile'):
+            self._remove_file(self.outFile)
 
     def _check_overwriting(self, inFile, outFile, analyze, args=[]):
         """
@@ -53,6 +61,7 @@ class TestGuiderImage(guiderTester.GuiderTester,unittest.TestCase):
         """Test GuiderImageAnalysis.analyzeDark()"""
         inFile = self.inDarkFile
         outFile = self.outDarkFile
+        self.outFile = outFile # for tearDown
         self.gi.analyzeDark(inFile, cmd=self.cmd)
         self.assertTrue(os.path.exists(outFile),'analyzeDark file write: %s'%outFile)
         self._check_overwriting(inFile,outFile,self.gi.analyzeDark)
@@ -60,19 +69,38 @@ class TestGuiderImage(guiderTester.GuiderTester,unittest.TestCase):
     def test_analyzeFlat(self):
         """Test GuiderImageAnalysis.analyzeFlat()"""
         inFile = self.inFlatFile
-        outFile = self.outFlatFile
+        self.outFile = self.outFlatFile
         self.gi.analyzeFlat(inFile,self.gState.gprobes,cmd=self.cmd)
-        self.assertTrue(os.path.exists(outFile),'analyzeFlat file write: %s'%outFile)
-        # TBD: some way to test that the correct fibers are identified?
-        for name,i in self.probeNames.items():
-        #    #self.assertEqual(i,self.gi.flatFibers[i].fiberid)
-            print i,self.gi.flatFibers[i].fiberid
-        self._check_overwriting(inFile,outFile,self.gi.analyzeFlat,[self.gState.gprobes])
+        self.assertTrue(os.path.exists(self.outFile),'analyzeFlat file write: %s'%self.outFile)
+
+        # TBD: I know this is a dumb test: we want to do a better analysis
+        # (maybe by hand in IRAF or something?) of the images and use that for
+        # our comparison here.
+        # NOTE TBD: In particular, the current expected results file is missing
+        # fiber 11 because it's damaged. If we fix the code to deal with that
+        # per #2357 we'll need a test that knows about that fiber!
+        result = pyfits.open(self.outFile)
+        expect = pyfits.open(self.flatExpect)
+        for x in result[6].data.names:
+            if x == 'fiber_type':
+                # Trouble testing numpy string arrays
+                self.assertTrue((result[6].data[x] == expect[6].data[x]).all(),'%s has a mismatch'%x)
+            elif x != 'rotStar2Sky' and x != 'ugriz' and x != 'ref_mag':
+                # !!!!!!!!!!!!!!!
+                # TBD: remove the if rotStar2Sky once we have a better test!
+                # The problem is rotStar2Sky was never saved for old flats.
+                # !!!!!!!!!!!!!!!
+                # TBD: don't have ugriz data for these to test, but could add some.
+                # Would have to pull it from the database.
+                # !!!!!!!!!!!!!!!
+                np.testing.assert_allclose(result[6].data[x],expect[6].data[x],err_msg='%s has a mismatch'%x)
+        self._check_overwriting(inFile,self.outFile,self.gi.analyzeFlat,[self.gState.gprobes])
 
     def test_analyzeFlat_ecam(self):
         """Test GuiderImageAnalysis.analyzeFlat()"""
         inFile = self.inFlatEcamFile
         outFile = self.outFlatEcamFile
+        self.outFile = outFile # for tearDown
         self.gi.camera = 'ecamera'
         self.gi.analyzeFlat(inFile,self.gState.gprobes,cmd=self.cmd)
         self.assertTrue(os.path.exists(outFile),'analyzeFlat_ecam file write: %s'%outFile)
@@ -82,12 +110,41 @@ class TestGuiderImage(guiderTester.GuiderTester,unittest.TestCase):
 
     def test_call(self):
         """Test GuiderImageAnalysis.__call__()"""
-        frameInfo = GuiderState.FrameInfo(-1,1,2,3)
+        self.outFile = self.outDataFile # for tearDown
+        frameInfo = GuiderState.FrameInfo(40,1,2,3)
         fibers = self._call_gi(self.inDataFile)
         self.gi.writeFITS(self.actorState.models, self.cmd, frameInfo, self.gState.gprobes)
 
-        for name,i in self.probeNames.items():
-            self.assertEqual(i,fibers[i].fiberid-1)
+        # TBD: I know this is a dumb test: we want to do a better analysis
+        # (maybe by hand in IRAF or something?) of the images and use that for
+        # our comparison here.
+        result = pyfits.open(self.outFile)
+        expect = pyfits.open(self.flatExpect)
+        for x in result[6].data.names:
+            if x == 'fiber_type':
+                # Trouble testing numpy string arrays
+                self.assertTrue((result[6].data[x] == expect[6].data[x]).all(),'%s has a mismatch'%x)
+            elif x != 'rotStar2Sky' and x != 'ugriz' and x != 'ref_mag':
+                # !!!!!!!!!!!!!!!
+                # TBD: remove the if rotStar2Sky once we have a better test!
+                # The problem is rotStar2Sky was never saved for old flats.
+                # !!!!!!!!!!!!!!!
+                # TBD: don't have ugriz data for these to test, but could add some.
+                # Would have to pull it from the database.
+                # !!!!!!!!!!!!!!!
+                np.testing.assert_allclose(result[6].data[x],expect[6].data[x],err_msg='%s has a mismatch'%x)
+
+        import matplotlib.pyplot as plt
+        plt.ion()
+        plt.figure()
+        plt.subplot(1,2,1)
+        plt.imshow(result[4].data,vmin=0)
+        plt.title('result')
+        plt.subplot(1,2,2)
+        plt.imshow(expect[4].data,vmin=0)
+        plt.title("expect")
+        raw_input('blah')
+
         self.assertFalse(True,'make a test!')
 
     def test_findStars_ecam(self):
@@ -106,13 +163,13 @@ class TestGuiderImage(guiderTester.GuiderTester,unittest.TestCase):
         """Test what happens when the ccdtemp is outside the setPoint spec for a dark."""
         self.gi.setPoint = self.setPoint_bad
         self._temp_run(self.inDarkFile,'CCD temp signifcantly different (>3.0) from setPoint: -40.1, expected -35.0')
-        
+
     def test_badSetPoint_image(self):
         """Test what happens when the ccdtemp is outside the setPoint spec for an image."""
         self.gi.setPoint = self.setPoint_bad
         self.gi.darkTemperature = self.setPoint_good
         self._temp_run(self.inDataFile,'CCD temp signifcantly different (>3.0) from setPoint: -40.1, expected -35.0')
-    
+
     def test_badSetPoint_dark_temp(self):
         """Test what happens when the ccdtemp is outside the dark temp spec for an image."""
         self.gi.darkTemperature = self.setPoint_bad
@@ -155,7 +212,7 @@ if __name__ == '__main__':
 
     suite = None
     # to test just one piece
-    # suite = unittest.TestLoader().loadTestsFromName('test_guiderImage.TestGuiderImage.test_call')
+    suite = unittest.TestLoader().loadTestsFromName('test_guiderImage.TestGuiderImage.test_call')
     # suite = unittest.TestLoader().loadTestsFromName('test_guiderImage.TestGuiderImage.test_analyzeFlat')
     # suite = unittest.TestLoader().loadTestsFromName('test_guiderImage.TestGuiderImage.test_analyzeFlat_ecam')
     # suite = unittest.TestLoader().loadTestsFromTestCase(TestGuiderImage)
