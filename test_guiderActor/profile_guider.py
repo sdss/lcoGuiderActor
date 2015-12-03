ba8f71121bd7a6b0c57065f9ecf74df177461910
parejkoj <parejkoj@localhost>
2015-12-03 19:23:11 +0000

    refactor hdulist stuff for profiling, make profiling work correctly, cleanup after tests better.

--- test_guiderActor/profile_guider.py
+++ test_guiderActor/profile_guider.py
@@ -2,11 +2,12 @@
 """Test the speed of guider image processing."""
 import cProfile
 import pstats
-import os
+import os,glob
 
 from actorcore import TestHelper
 
 from guiderActor import masterThread
+from guiderActor.gimg import guiderImage
 import guiderActor.myGlobals as myGlobals
 
 import guiderTester
@@ -21,7 +22,10 @@ dataFile = 'gcam/gimg-0040.fits.gz'
 
 helper = guiderTester.GuiderTester()
 helper.setUp()
-actor = TestHelper.FakeActor('guider','guiderActor')
+# force the gi instance to be at location=APO.
+helper.gi = guiderImage.GuiderImageAnalysis(helper.setPoint_good,'APO')
+# setup to use all 16 "real" probes
+helper._init_probes_gimg_0040()
 updateModel('mcp',TestHelper.mcpState['boss_science'])
 helper.actorState.bypassDark = False
 
@@ -31,17 +35,21 @@ helper.gi.analyzeFlat(flatFile,helper.gState.gprobes,cmd=helper.cmd)
 helper.gState.cmd = helper.cmd
 
 def timeit_helper():
-    masterThread.guideStep(actor,None,helper.cmd,helper.gState,dataFile,False,helper.gi)
+    masterThread.guideStep(helper.actor,None,helper.cmd,helper.gState,dataFile,False,helper.gi)
     os.remove('gcam/proc-gimg-0040.fits.gz')
 
 # Paste this line for a multi-pass timing run in ipython, after pasting the above.
 # %timeit timeit_helper()
 
+def cleanup():
+    for filename in glob.glob('?cam/proc-*.fits.gz'):
+        os.remove(filename)
 
 prof = cProfile.Profile()
 #result = prof.runcall(helper.gi,helper.cmd,dataFile,helper.gState.gprobes,-40)
-result = prof.runcall(masterThread.guideStep,actor,None,helper.cmd,helper.gState,dataFile,False,helper.gi)
+result = prof.runcall(masterThread.guideStep,helper.actor,None,helper.cmd,helper.gState,dataFile,False,helper.gi)
+cleanup()
 prof.dump_stats('guideStep.profile')
 stats = pstats.Stats('guideStep.profile')
 stats.strip_dirs()
-stats.sort_stats('cumtime').print_stats(20)
+stats.sort_stats('cumtime').print_stats(25)
