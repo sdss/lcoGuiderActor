3fa57b36f15c6d37d9d30584e0ba4f12c2a9b715
José Sánchez-Gallego <gallegoj@uw.edu>
2016-06-06 12:48:15 -0600

    added a test mode for testing (uses localhost)

--- python/guiderActor/guiderActor_main.py
+++ python/guiderActor/guiderActor_main.py
@@ -5,7 +5,7 @@ from guiderActor import Msg, GuiderActor
 
 # start a new actor
 if __name__ == "__main__":
-    #LCOHACK: since we don't have a proper domain name yet, 
+    #LCOHACK: since we don't have a proper domain name yet,
     #LCOHACK: force the location to lco.
-    guider = GuiderActor.GuiderActor.newActor(location='lco')
+    guider = GuiderActor.GuiderActor.newActor()
     guider.run(Msg=Msg)
