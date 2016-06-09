3714380b54508be8565cbb7dc58490476e6a4fb6
José Sánchez-Gallego <gallegoj@uw.edu>
2016-06-09 15:35:58 -0600

    removed LCOHACK comment

--- python/guiderActor/guiderActor_main.py
+++ python/guiderActor/guiderActor_main.py
@@ -5,7 +5,5 @@ from guiderActor import Msg, GuiderActor
 
 # start a new actor
 if __name__ == "__main__":
-    #LCOHACK: since we don't have a proper domain name yet,
-    #LCOHACK: force the location to lco.
     guider = GuiderActor.GuiderActor.newActor()
     guider.run(Msg=Msg)
