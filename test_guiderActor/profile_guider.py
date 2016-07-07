98935bfbf978b03522dd4b9bbc53b01e0b9535d8
parejkoj <parejkoj@d97-176.lco.cl>
2016-07-07 01:46:15 +0000

    Trying to get profile_guider running with the new guiderImages repo.
    There were a bunch of testing API changes that profile_guider didn't keep up
    with and the files were moved to a separate repo.
    
    It's not quite working (gave me an exception when writing the FITS file), but
    it should be close.
    
    Also did pep8 cleanups.
    
    (the goal here was a quick test of PyGuide performance for LSST)

--- test_guiderActor/profile_guider.py
+++ test_guiderActor/profile_guider.py
@@ -2,7 +2,8 @@
 """Test the speed of guider image processing."""
 import cProfile
 import pstats
-import os,glob
+import os
+import glob
 
 from actorcore import TestHelper
 
@@ -12,42 +13,50 @@ import guiderActor.myGlobals as myGlobals
 
 import guiderTester
 
-def updateModel(name,model):
+
+def updateModel(name, model):
     """Update the named actorState model with new parameters."""
-    myGlobals.actorState.models[name] = TestHelper.Model(name,model)
+    myGlobals.actorState.models[name] = TestHelper.Model(name, model)
 
-darkFile = 'gcam/gimg-0001.fits.gz'
-flatFile = 'gcam/gimg-0003.fits.gz'
-dataFile = 'gcam/gimg-0040.fits.gz'
+mjd = 57357
 
-helper = guiderTester.GuiderTester()
-helper.setUp()
+darkFile = guiderTester.getTestFile('gcam/', mjd, 'gimg-0001.fits.gz')
+flatFile = guiderTester.getTestFile('gcam/', mjd, 'gimg-0003.fits.gz')
+dataFile = guiderTester.getTestFile('gcam/', mjd, 'gimg-0040.fits.gz')
+
+gTester = guiderTester.GuiderTester()
+gTester.attachCmdSets = False
+gTester.setUp()
 # force the gi instance to be at location=APO.
-helper.gi = guiderImage.GuiderImageAnalysis(helper.setPoint_good,'APO')
+gTester.gi = guiderImage.GuiderImageAnalysis(gTester.setPoint_good, 'APO')
 # setup to use all 16 "real" probes
-helper._init_probes_gimg_0040()
-updateModel('mcp',TestHelper.mcpState['boss_science'])
-helper.actorState.bypassDark = False
+gTester.init_probes(mjd, 7660, 57356, 1, camera='gcam')
+updateModel('mcp', TestHelper.mcpState['boss_science'])
+gTester.actorState.bypassDark = False
 
 # pre-process the dark and flat.
-helper.gi.analyzeDark(darkFile,cmd=helper.cmd)
-helper.gi.analyzeFlat(flatFile,helper.gState.gprobes,cmd=helper.cmd)
-helper.gState.cmd = helper.cmd
+gTester.gi.analyzeDark(darkFile, cmd=gTester.cmd)
+gTester.gi.analyzeFlat(flatFile, gTester.gState.gprobes, cmd=gTester.cmd)
+gTester.gState.cmd = gTester.cmd
 
-def timeit_helper():
-    masterThread.guideStep(helper.actor,None,helper.cmd,helper.gState,dataFile,False,helper.gi)
+
+def timeit_gTester():
+    masterThread.guideStep(gTester.actor, None, gTester.cmd,
+                           gTester.gState, dataFile, False, gTester.gi)
     os.remove('gcam/proc-gimg-0040.fits.gz')
 
 # Paste this line for a multi-pass timing run in ipython, after pasting the above.
-# %timeit timeit_helper()
+# %timeit timeit_gTester()
+
 
 def cleanup():
     for filename in glob.glob('?cam/proc-*.fits.gz'):
         os.remove(filename)
 
 prof = cProfile.Profile()
-#result = prof.runcall(helper.gi,helper.cmd,dataFile,helper.gState.gprobes,-40)
-result = prof.runcall(masterThread.guideStep,helper.actor,None,helper.cmd,helper.gState,dataFile,False,helper.gi)
+# result = prof.runcall(gTester.gi,gTester.cmd,dataFile,gTester.gState.gprobes,-40)
+result = prof.runcall(masterThread.guideStep, gTester.actor, None, gTester.cmd,
+                      gTester.gState, dataFile, False, gTester.gi)
 cleanup()
 prof.dump_stats('guideStep.profile')
 stats = pstats.Stats('guideStep.profile')
