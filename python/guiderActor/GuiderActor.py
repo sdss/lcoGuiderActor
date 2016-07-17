4feed2dfe7ae5a9e2903bfcd9aa6288d0d9bceaa
José Sánchez-Gallego <gallegoj@uw.edu>
2016-07-17 01:59:07 -0400

    removed spaces

--- python/guiderActor/GuiderActor.py
+++ python/guiderActor/GuiderActor.py
@@ -259,7 +259,7 @@ class GuiderActorLCO(GuiderActor):
             cmd.warn('text="Secondary mirror state: {0}. Aborted guiding."'
                      .format(mirror2CmdState))
             return False
-        
+
         # LCOHACK: disabling this part untils the scaling ring is connected
         # Checks the state of the scaling ring. Fails if it's moving.
         # scaleCmdState = tccModel.keyVarDict['threadringState'][0]
