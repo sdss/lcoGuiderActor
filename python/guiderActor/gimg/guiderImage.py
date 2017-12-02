d2491594dd4b091fefb23d454003e2bf008ef961
José Sánchez-Gallego <gallegoj@uw.edu>
2017-12-01 17:28:28 -0800

    Manually adds rotStar2Sky to tritiums

--- python/guiderActor/gimg/guiderImage.py
+++ python/guiderActor/gimg/guiderImage.py
@@ -1263,7 +1263,9 @@ class GuiderImageAnalysis(object):
                 fiber_x = (stampFrameCoords[0] + star.xyCtr[0]) / BIN - 0.25
                 fiber_y = (stampFrameCoords[1] + star.xyCtr[1]) / BIN - 0.25
                 fiber = Fiber(gprobe.id, fiber_x, fiber_y, 0, -1, label=-1)
+
                 fiber.gProbe = gprobe
+                fiber.gProbe.rotStar2Sky = 0.0
 
                 fibers.append(fiber)
 
