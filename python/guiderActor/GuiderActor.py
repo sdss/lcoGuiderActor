5340b93b3d8584c2787899c4dd58489b975bda75
José Sánchez-Gallego <gallegoj@uw.edu>
2018-06-19 12:01:09 +0900

    Remove references to MCP and APO actors.

--- python/guiderActor/GuiderActor.py
+++ python/guiderActor/GuiderActor.py
@@ -98,7 +98,7 @@ class GuiderActor(actorcore.Actor.SDSSActor):
         # Load other actor's models so we can send it commands
         # And ours: we use the models to generate the FITS cards.
         self.models = {}
-        for actor in ['gcamera', 'ecamera', 'mcp', 'platedb', 'sop', 'tcc', 'guider', 'apo']:
+        for actor in ['gcamera', 'ecamera', 'platedb', 'sop', 'tcc', 'guider']:
             self.models[actor] = opscore.actor.model.Model(actor)
 
         self.actorState = actorcore.Actor.ActorState(self, self.models)
